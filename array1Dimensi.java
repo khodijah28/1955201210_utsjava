f0d2feddeae8b62aad374da1da3f47b58ef2124a
khodijah28 <67194273+khodijah28@users.noreply.github.com>
2020-08-11 21:05:16 +0700

    Add files via upload

---
+++ array1Dimensi.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugasss;
+
+/**
+ *
+ * @author FITRI CAHYANTI
+ */
+public class array1Dimensi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int[]month;
+        month = new int [12];
+        month[0]=31;
+        month[1]=28;
+        month[2]=31;
+        month[3]=30;
+        month[4]=31;
+        month[5]=30;
+        month[6]=31;
+        month[7]=31;
+        month[8]=30;
+        month[9]=31;
+        month[10]=30;
+        month[11]=31;
+       
+        System.out.println("month 2 is"+ month[2] + "days");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
---
+++ multidimensi.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugasss;
+
+
+
+/**
+ *
+ * @author FITRI CAHYANTI
+ */
+public class multidimensi {
+    public static void main(String[] args) {
+        int data [][]= new int [2][2];
+      data[0][0]=1;
+        data[0][1]=2;
+          data[1][0]=3;
+            data[1][1]=4;
+        System.out.println(data[0][0]+"");
+        System.out.println(data[0][1]+"");
+        System.out.println(data[1][0]+"");
+        System.out.println(data[1][1]+"");
+        
+    }
+}
